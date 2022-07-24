4cefb887d166380b3dafa45277832107b78c0523
Sirun Hayrapetyan <sirun.hayrapetyan@mail.ru>
2022-07-24 23:15:58 +0400

    Initial commit

---
+++ cypress/support/data.js
@@ -0,0 +1,12 @@
+module.exports = {
+        userID() {
+            function s4() {
+                return Math.floor((1 + Math.random()) * 0x10000)
+                    .toString(16)
+                    .substring(1);
+            }
+            return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+                s4() + '-' + s4() + s4() + s4();
+        }
+    }
+    //have a problem with the beautifier, will correct it later
